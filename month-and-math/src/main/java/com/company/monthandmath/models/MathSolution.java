19b649f921e6a38df87af0fad06e0fa13d0c71f1
scouring pad <hcc780921@gmail.com>
2022-07-07 20:18:28 -0700

    @model Month constructors setandget toString equalandhasCode created

--- month-and-math/src/main/java/com/company/monthandmath/models/MathSolution.java
+++ month-and-math/src/main/java/com/company/monthandmath/models/MathSolution.java
@@ -1,4 +1,6 @@
 package com.company.monthandmath.models;
 
 public class MathSolution {
+
+
 }
--- month-and-math/src/main/java/com/company/monthandmath/models/Month.java
+++ month-and-math/src/main/java/com/company/monthandmath/models/Month.java
@@ -1,4 +1,51 @@
 package com.company.monthandmath.models;
 
+import java.util.Objects;
+
 public class Month {
+
+    String number;
+    String monthName;
+
+    public Month(String number, String monthName) {
+        this.number = number;
+        this.monthName = monthName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getMonthName() {
+        return monthName;
+    }
+
+    public void setMonthName(String monthName) {
+        this.monthName = monthName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Month month = (Month) o;
+        return Objects.equals(number, month.number) && Objects.equals(monthName, month.monthName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, monthName);
+    }
+
+    @Override
+    public String toString() {
+        return "Month{" +
+                "number='" + number + '\'' +
+                ", monthName='" + monthName + '\'' +
+                '}';
+    }
 }
