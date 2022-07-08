91136bfed220e5f602db0d822de6988d6fc8bc24
scouring pad <hcc780921@gmail.com>
2022-07-07 20:04:22 -0700

    @RequestMapping @ResponseStatus added in controllers

--- month-and-math/src/main/java/com/company/monthandmath/controller/MathSolutionController.java
+++ month-and-math/src/main/java/com/company/monthandmath/controller/MathSolutionController.java
@@ -1,4 +1,18 @@
 package com.company.monthandmath.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
 public class MathSolutionController {
+//add
+@RequestMapping(value="/add",method= RequestMethod.GET)
+@ResponseStatus(HttpStatus.OK)
+    public MathSolution add(@Valid @RequestBody MathSolution mathSolution){
+ 
+}
+
 }
--- month-and-math/src/main/java/com/company/monthandmath/controller/MonthController.java
+++ month-and-math/src/main/java/com/company/monthandmath/controller/MonthController.java
@@ -1,4 +1,13 @@
 package com.company.monthandmath.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MonthController {
+    @RequestMapping(value="/month",method= RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
 }
