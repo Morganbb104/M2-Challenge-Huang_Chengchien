5ef94798105f7537d85b2019f0cfb4f0a554a39d
scouring pad <hcc780921@gmail.com>
2022-07-07 19:00:09 -0700

    month-and-math spring initializr added

---
+++ month-and-math/src/test/java/com/company/monthandmath/MonthAndMathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.monthandmath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthAndMathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
