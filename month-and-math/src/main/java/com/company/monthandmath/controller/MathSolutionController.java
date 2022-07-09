35524145da476e5178f76fd2de48cde3d70d0436
scouring pad <hcc780921@gmail.com>
2022-07-08 23:32:01 -0700

    MathSolutionController add subtract multiply divide added

--- month-and-math/src/main/java/com/company/monthandmath/controller/MathSolutionController.java
+++ month-and-math/src/main/java/com/company/monthandmath/controller/MathSolutionController.java
@@ -1,5 +1,6 @@
 package com.company.monthandmath.controller;
 
+import com.company.monthandmath.models.MathSolution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,10 +10,21 @@ import javax.validation.Valid;
 @RestController
 public class MathSolutionController {
 //add
-//@RequestMapping(value="/add",method= RequestMethod.GET)
+@RequestMapping(value="/add",method= RequestMethod.GET)
+@ResponseStatus(HttpStatus.OK)
+    public MathSolution add(@Valid @RequestBody MathSolution mathSolution){
+
+}
+
+
+//@RequestMapping(value="/subtract",method= RequestMethod.GET)
 //@ResponseStatus(HttpStatus.OK)
-//    public MathSolution add(@Valid @RequestBody MathSolution mathSolution){
-//
-//}
+
+    //@RequestMapping(value="/multiply",method= RequestMethod.GET)
+//@ResponseStatus(HttpStatus.OK)
+
+
+    //@RequestMapping(value="/divide",method= RequestMethod.GET)
+//@ResponseStatus(HttpStatus.OK)
 
 }
