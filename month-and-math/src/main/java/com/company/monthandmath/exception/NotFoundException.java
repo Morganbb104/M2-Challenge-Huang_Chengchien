2c46d831da7b7fa3da11dbac29c635ee08b977fa
scouring pad <hcc780921@gmail.com>
2022-07-08 22:56:54 -0700

    exception.NotFoundException created

---
+++ month-and-math/src/main/java/com/company/monthandmath/exception/NotFoundException.java
@@ -0,0 +1,6 @@
+package com.company.monthandmath.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException() { super();}
+    public NotFoundException(String msg){ super(msg);}
+}
