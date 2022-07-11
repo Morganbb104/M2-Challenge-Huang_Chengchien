5fe5367135b32a3b866065dbda8d7c1698eea04f
scouring pad <hcc780921@gmail.com>
2022-07-10 19:56:27 -0700

    MonthController RandomMathController @Test completed

--- month-and-math/src/test/java/com/company/monthandmath/controller/MonthControllerTest.java
+++ month-and-math/src/test/java/com/company/monthandmath/controller/MonthControllerTest.java
@@ -10,10 +10,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MonthController.class)
@@ -31,12 +33,12 @@ public class MonthControllerTest {
     @Test
     public void shouldReturnTheExpectedMonthNameByNumber() throws Exception{
         Month outputMonth = new Month(9,"September");
-//        String outputJson = mapper.writeValueAsString(outputMonth);
+        String outputJson = mapper.writeValueAsString(outputMonth);
 
-        mockmvc.perform(get("http://localhost:8080/month/9"))
+        mockmvc.perform(get("/month/9"))
                 .andDo(print())
-                .andExpect(status().isOk());
-//                .andExpect(content().string(outputJson));
+                .andExpect(status().isOk())
+                .andExpect(content().string(outputJson));
 
 
     }
--- month-and-math/src/test/java/com/company/monthandmath/controller/RandomMonthControllerTest.java
+++ month-and-math/src/test/java/com/company/monthandmath/controller/RandomMonthControllerTest.java
@@ -4,8 +4,12 @@ import com.company.monthandmath.models.Month;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
@@ -13,6 +17,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(MonthController.class)
 public class RandomMonthControllerTest {
     @Autowired
     private MockMvc mockmvc;
@@ -26,13 +32,14 @@ public class RandomMonthControllerTest {
 
     @Test
     public void shouldReturnExpectedMonthNameByRandomNumber() throws Exception{
-        Month outputMonth = new Month(9,"September");
+//        Month outputMonth = new Month(9,"September");
 //        String outputJson = mapper.writeValueAsString(outputMonth);
 
-        mockmvc.perform(get("http://localhost:8080/randomMonth"))
+        mockmvc.perform(get("/randomMonth"))
                 .andDo(print())
                 .andExpect(status().isOk());
-                .andExpect(content().string(outputJson));
+//                .andExpect(MockMvcResultMatchers.content().string(outputJson));
+//                .andExpect(content().string(outputJson));
 
 
     }
