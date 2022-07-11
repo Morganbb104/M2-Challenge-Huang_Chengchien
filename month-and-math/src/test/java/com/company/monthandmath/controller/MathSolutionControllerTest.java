cabcb39c58b13ae203b3d3f067aa1a3397113216
scouring pad <hcc780921@gmail.com>
2022-07-11 07:18:24 -0700

    MathSolutionController @Test completed

--- month-and-math/src/test/java/com/company/monthandmath/controller/MathSolutionControllerTest.java
+++ month-and-math/src/test/java/com/company/monthandmath/controller/MathSolutionControllerTest.java
@@ -12,15 +12,18 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-
 @RunWith(SpringRunner.class)
-@WebMvcTest(MonthController.class)
+@WebMvcTest(MathSolutionController.class)
 public class MathSolutionControllerTest {
     @Autowired
     private MockMvc mockmvc;
@@ -52,20 +55,59 @@ public class MathSolutionControllerTest {
 
     @Test
     public void shouldReturnDifferenceOfTwoNumbers() throws Exception{
+        MathSolution mathSolution = new MathSolution();
+        mathSolution.setOperand1(6);
+        mathSolution.setOperand2(3);
+        String inputString = mapper.writeValueAsString(mathSolution);
 
+        MathSolution output = new MathSolution(6,3,"subtract",3);
+        String outputString = mapper.writeValueAsString(output);
+
+        this.mockmvc.perform(post("/subtract")
+                        .content(inputString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(outputString));
 
 
     }
 
     @Test
     public void shouldReturnProductOfTwoNumbers() throws Exception{
+        MathSolution mathSolution = new MathSolution();
+        mathSolution.setOperand1(6);
+        mathSolution.setOperand2(3);
+        String inputString = mapper.writeValueAsString(mathSolution);
 
+        MathSolution output = new MathSolution(6,3,"multiply",18);
+        String outputString = mapper.writeValueAsString(output);
+
+        this.mockmvc.perform(post("/multiply")
+                        .content(inputString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(outputString));
 
 
     }
     @Test
     public void shouldReturnQuotientOfTwoNumbers() throws Exception{
+        MathSolution mathSolution = new MathSolution();
+        mathSolution.setOperand1(6);
+        mathSolution.setOperand2(3);
+        String inputString = mapper.writeValueAsString(mathSolution);
 
+        MathSolution output = new MathSolution(6,3,"divide",2);
+        String outputString = mapper.writeValueAsString(output);
+
+        this.mockmvc.perform(post("/divide")
+                        .content(inputString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(outputString));
 
 
     }
