8b7c788a6b86a12a3e3812e63bf9e8d4a3acb3de
scouring pad <hcc780921@gmail.com>
2022-07-11 07:20:36 -0700

    Deleted umused import

--- month-and-math/src/test/java/com/company/monthandmath/controller/MathSolutionControllerTest.java
+++ month-and-math/src/test/java/com/company/monthandmath/controller/MathSolutionControllerTest.java
@@ -12,10 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
